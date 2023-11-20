ea4caec2bc35bbc02e2f7329ca7e03d6e1b0e649
Vivek <vivekcse.in@gmail.com>
2023-11-20 19:44:36 +0530

    end point redirects

--- src/utils/endpoints.ts
+++ src/utils/endpoints.ts
@@ -1,5 +1,6 @@
 // export const endpoint_layout = 'http://127.0.0.1:3000/api/layout'
-export const endpoint_layout = 'http://vivekcse.in/api/layout'
+// export const endpoint_layout = 'http://vivekcse.in/api/layout'
+export const endpoint_layout = 'https://vivekcse-8m0g87pb1-viveks-projects-13375c0a.vercel.app/api/layout'
 
 
 export const hostingLink = 'https://vivekcse.github.io/frnz';
