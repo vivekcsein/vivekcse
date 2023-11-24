7054236335716e92582440cc882d1796113dbdb6
Vivek <vivekcse.in@gmail.com>
2023-11-24 13:57:06 +0530

    change localhost to production mode

--- src/utils/endpoints.ts
+++ src/utils/endpoints.ts
@@ -1,8 +1,8 @@
-export const endpoint_layout = 'http://127.0.0.1:3000/api/layout'
+// export const endpoint_layout = 'http://127.0.0.1:3000/api/layout'
 // export const endpoint_layout = 'https://vivekcsein.github.io/apidata/layout.json'
-// export const endpoint_layout = 'https://vivekcse.in/api/layout';
+export const endpoint_layout = 'https://vivekcse.in/api/layout';
 
 
 // export const hostingLink = 'https://vivekcse.github.io/frnz';
-export const hostingLink = 'http://localhost:3000';
-// export const hostingLink = 'https://vivekcse.in';
+// export const hostingLink = 'http://localhost:3000';
+export const hostingLink = 'https://vivekcse.in';
